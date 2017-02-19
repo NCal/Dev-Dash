e148a0aecac7502ea81bfc884c908833b35204f9
NCal <nathanbroaddus@gmail.com>
2017-02-19 11:24:13 -0500

    [Dev-Dash] adding new greyscale icons, removing unused components, cleaning css, refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { Row, Column } from 'react-foundation';
 import Foundation from 'react-foundation';
-import $ from 'jQuery';
-// import Quote_array from '../data/quote_array.js';
-// import audio from '../data/audio_array.js';
-import User_Links from './user_links.js';
+// import $ from 'jQuery';
 import Search from './Search.js';
 import Main_Date from './Date.js';
 import News from './News.js';
@@ -32,18 +29,12 @@ class App extends Component {
 
   render(){
     return (
-      <div>
       <div className="application">
-      {/*  <Row className="display">
-          <Column  large={12}></Column>
-          <User_Links/> */}
-       {/*  </Row>*/}
-       <Search/>
-       <Main_Date/>
-       <News/>
-       <Weather/>
-       <Docs/>
-        </div>
+         <Search/>
+         <Main_Date/>
+         <News/>
+         <Weather/>
+         <Docs/>
       </div>)
   }
 }
---
+++ src/data/docs_data.js
@@ -0,0 +1,19 @@
+let docs_data = [
+		{symbol: 'R', logo: 'src/assets/docs_logos/grey/react.png', name: 'React', url: 'https://facebook.github.io/react/docs/hello-world.html'},
+		{symbol: 'JS', logo: 'src/assets/docs_logos/grey/javascript.png', name: 'JavaScript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+		{symbol: 'CSS', logo: 'src/assets/docs_logos/grey/css.png', name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference'},
+		{symbol: 'jQ', logo: 'src/assets/docs_logos/grey/jquery.png', name: 'Jquery', url: 'http://api.jquery.com/'},
+		{symbol: 'RB', logo: 'src/assets/docs_logos/grey/ruby.png', name: 'Ruby', url: 'http://ruby-doc.org/'},
+		{symbol: 'PHP', logo: 'src/assets/docs_logos/grey/php.png', name: 'PHP', url: 'http://php.net/docs.php'},
+		{symbol: 'A', logo: 'src/assets/docs_logos/grey/angular.png', name: 'Angular', url: 'https://docs.angularjs.org/api'},
+		{symbol: 'R', logo: 'src/assets/docs_logos/grey/rails.png', name: 'Ruby on Rails', url: 'http://api.rubyonrails.org/'},
+		{symbol: 'ND', logo: 'src/assets/docs_logos/grey/node.png', name: 'Node', url: 'https://nodejs.org/api/'},
+		{symbol: 'B', logo: 'src/assets/docs_logos/grey/bootstrap.png', name: 'Bootstrap', url: 'http://getbootstrap.com/'},
+		{symbol: 'F', logo: 'src/assets/docs_logos/grey/foundation.png', name: 'Foundation', url: 'http://foundation.zurb.com/sites/docs/'},
+		{symbol: 'NPM', logo: 'src/assets/docs_logos/grey/npm.png', name: 'NPM', url: 'https://www.npmjs.com/'},
+		{symbol: 'W', logo: 'src/assets/docs_logos/grey/webpack.png', name: 'Webpack', url: 'http://webpack.github.io/docs/'}
+		// {symbol: '+', logo: 'src/assets/docs_logos/grey/add.png', name: 'Add', url: '#'},
+
+
+];
+export default docs_data;
