273f1a2a696b41fdbef70aabaefe855ccdf2fe08
Nathan Broaddus <nbroaddus@MV-109-nbroadus-2.local>
2017-02-18 21:11:11 -0500

    [Dev-Dash] refactoring docs component, styles, bg image

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -5,19 +5,20 @@ import $ from 'jQuery';
 
 
 let data = [
-		{symbol: 'R', name: 'React', url: 'https://facebook.github.io/react/docs/hello-world.html'},
-		{symbol: 'JS', name: 'JavaScript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
-		{symbol: 'CSS', name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference'},
-		{symbol: 'jQ', name: 'Jquery', url: 'http://api.jquery.com/'},
-		{symbol: 'RB', name: 'Ruby', url: 'http://ruby-doc.org/'},
-		{symbol: 'PHP', name: 'PHP', url: 'http://php.net/docs.php'},
-		{symbol: 'A', name: 'Angular', url: 'https://docs.angularjs.org/api'},
-		{symbol: 'R', name: 'Rails', url: 'http://api.rubyonrails.org/'},
-		{symbol: 'ND', name: 'Node', url: 'https://nodejs.org/api/'},
-		{symbol: 'B', name: 'Bootstrap', url: 'http://getbootstrap.com/'},
-		{symbol: 'F', name: 'Foundation', url: 'http://foundation.zurb.com/sites/docs/'},
-		{symbol: 'NPM', name: 'NPM', url: 'https://www.npmjs.com/'},
-		{symbol: 'W', name: 'Webpack', url: 'http://webpack.github.io/docs/'},
+		{symbol: 'R', logo: 'src/assets/docs_logos/react.png', name: 'React', url: 'https://facebook.github.io/react/docs/hello-world.html'},
+		{symbol: 'JS', logo: 'src/assets/docs_logos/javascript.png', name: 'JavaScript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+		{symbol: 'CSS', logo: 'src/assets/docs_logos/css.png', name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/Reference'},
+		{symbol: 'jQ', logo: 'src/assets/docs_logos/jquery.png', name: 'Jquery', url: 'http://api.jquery.com/'},
+		{symbol: 'RB', logo: 'src/assets/docs_logos/ruby.png', name: 'Ruby', url: 'http://ruby-doc.org/'},
+		{symbol: 'PHP', logo: 'src/assets/docs_logos/php.png', name: 'PHP', url: 'http://php.net/docs.php'},
+		{symbol: 'A', logo: 'src/assets/docs_logos/angular.png', name: 'Angular', url: 'https://docs.angularjs.org/api'},
+		{symbol: 'R', logo: 'src/assets/docs_logos/rails.png', name: 'Ruby on Rails', url: 'http://api.rubyonrails.org/'},
+		{symbol: 'ND', logo: 'src/assets/docs_logos/node.png', name: 'Node', url: 'https://nodejs.org/api/'},
+		{symbol: 'B', logo: 'src/assets/docs_logos/bootstrap.png', name: 'Bootstrap', url: 'http://getbootstrap.com/'},
+		{symbol: 'F', logo: 'src/assets/docs_logos/foundation.png', name: 'Foundation', url: 'http://foundation.zurb.com/sites/docs/'},
+		{symbol: 'NPM', logo: 'src/assets/docs_logos/npm.png', name: 'NPM', url: 'https://www.npmjs.com/'},
+		{symbol: 'W', logo: 'src/assets/docs_logos/webpack.png', name: 'Webpack', url: 'http://webpack.github.io/docs/'},
+
 ];
 
 class Docs extends Component {
@@ -52,9 +53,9 @@ class Docs extends Component {
 		for (let i = 0; i< data.length; i++){
 
 			if ( data[i].name.toLowerCase().indexOf(search.toLowerCase()) === -1){
-				$('.doc_bloc h5[title="'+data[i].name+'"]').hide();
+				$('.doc_bloc img[title="'+data[i].name+'"]').parent().hide();
 			}  else {
-				$('.doc_bloc h5[title="'+data[i].name+'"]').show();
+				$('.doc_bloc img[title="'+data[i].name+'"]').parent().show();
 			}
 		}
 	}
@@ -70,12 +71,13 @@ class Docs extends Component {
 			return (
 				<div className="docs_component">
 				<p style={{cursor: 'pointer'}} onClick={this.Toggle}>Hide</p>
-				<input type="text" className="docs_search_input" onChange={this.handleInput}/>
+				<input type="text" className="docs_search_input" placeholder="search" onChange={this.handleInput}/>
 				<div className="inner_scroll">
 				
 				{data.map(function(thang, i){
 					return <a key={i} style={{color: '#fff'}} href={thang.url}><Column large={3} className="doc_bloc">
-						<h5 title={thang.name}>{thang.symbol}</h5>
+						{/*<h5 title={thang.name}>{thang.symbol}</h5>*/}
+						<img className="doc_logo" title={thang.name} src={thang.logo}/>
 					</Column></a>
 				})}
 	
