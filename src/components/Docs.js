94b61b40ad1ebba8f7320f631b31a11aeb15f322
Nathan Broaddus <nathanbroaddus@gmail.com>
2017-02-22 17:03:38 -0500

    [Dev-Dash] refactoring

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Column } from 'react-foundation';
 import Foundation from 'react-foundation';
-import $ from 'jQuery';
 import DocsSearch from './DocsSearch.js';
 import docsData from '../data/docsData.js';
 
@@ -14,17 +13,17 @@ class Docs extends Component {
       this.saveInput = this.saveInput.bind(this);
       this.handleCustomDocs = this.handleCustomDocs.bind(this);
       this.docDelete = this.docDelete.bind(this);
+      this.localDocDelete = this.localDocDelete.bind(this);
       
       this.state = {
          mode: 'off',
          docInput: null,
-         customDocAmount: 0,
          customDocs: false,
       };
    }
 
    componentDidMount() {
-   	console.log(Docs);
+   	// console.log(Docs);
    	this.handleCustomDocs();
 
    	if (localStorage.customDocAmount === "0"){
@@ -33,7 +32,7 @@ class Docs extends Component {
    }
 
    handleCustomDocs(){
-   	console.log('handle custom docs');
+   	// console.log('handle custom docs');
 		if (localStorage.customDocAmount !== undefined){
 			let customDocs = JSON.parse(localStorage.customDocs);
 			// console.log('we have custom docs here:', customDocs);
@@ -54,53 +53,38 @@ class Docs extends Component {
    }
 
    addDoc(){
-   	// console.log('add doc');
    	this.setState({
    		mode: 'add'
    	});
    }
 
    subDoc(){
-   	// console.log('add doc');
    	this.setState({
    		mode: 'sub'
    	});
    }
 
    docDelete(e){
-   	let self = this;
-   	console.log('doc delete');
-   	console.log(e);
-   	var elToRemove = e.target.parentElement.innerText;
-   	// console.log('thing to remove:', e.target.parentElement.innerText);
-   	
+		let self = this;
+		this.localDocDelete(e);
+   }
+
+   localDocDelete(e){
+   	let elToRemove = e.target.parentElement.innerText;
+
    	docsData.forEach(function(doc, i){
-   		
-			// if (doc.name === elToRemove){
-				// e.target.parentElement.remove();
-			 if (doc.type === 'added' && doc.name === e.target.parentElement.innerText){
+			 if (doc.type === 'added' && doc.name === elToRemove){
 			 	e.target.parentElement.remove();
-			 	console.log(doc.type);
-				console.log(docsData);
-				console.log(doc);
-				console.log(docsData.indexOf(doc));
 				let startIndex = docsData.indexOf(doc);
-				docsData.splice(startIndex, 1);
 				let localDocAmount = parseInt(localStorage.customDocAmount);
+
+				docsData.splice(startIndex, 1);
 				localStorage.customDocAmount = localDocAmount -=1;
 				localStorage.customDocs = JSON.stringify(docsData.slice(0, parseInt(localStorage.customDocAmount)));
 
-				if (localStorage.customDocAmount === "0"){
-					self.setState({customDocs: false});
-				}
+				if (localStorage.customDocAmount === "0"){ self.setState({customDocs: false});}
 			}
-
-
    	});
-   }
-
-   handleInput(e){
-   	let input = e.target.value;
    }
 
    saveInput(){
@@ -122,31 +106,20 @@ class Docs extends Component {
 	   		customDocs: true
 			});
 
-			if (localStorage.customDocAmount === undefined){
-				// console.log('custom doc amount was undefined');
-				localStorage.customDocAmount = 1;
-			} else {
-				// console.log('adding to custom docs');
+			if (localStorage.customDocAmount === undefined){ localStorage.customDocAmount = 1;} 
+			else {
 				let localDocAmount = parseInt(localStorage.customDocAmount);
 				localStorage.customDocAmount = localDocAmount +=1;
-				// console.log('local storage custom doc amount:',localStorage.customDocAmount);
 			}
 
 			localStorage.customDocs = JSON.stringify(docsData.slice(0, parseInt(localStorage.customDocAmount)));
-			// console.log(JSON.parse(localStorage.customDocs));
-			// console.log('local docs', localStorage.customDocs);
-			// console.log(' new input was saved');
 
    	} else {
-	   	console.log('no input was saved');
+	   	// console.log('no input was saved');
 	   	this.setState({
 	   		mode: 'on'
 			});
    	}
-
-
-
-		
    }
 
    render(){
@@ -162,18 +135,19 @@ class Docs extends Component {
             		<Column large={3} className="doc_bloc" style={{cursor: 'pointer'}} onClick={this.subDoc}>
                      <img className="doc_logo" title="sub" src="src/assets/docs_logos/grey/sub.png"/>
                   </Column>
-                 {docsData.map(function(thang, i){
-                 	if (thang.type ==="added" ){
-                 		return <a key={i} style={{color: '#fff'}} href={thang.url}><Column large={3} className="doc_bloc">
-                 		   <h3 title={thang.name}>{thang.symbol}</h3>
+                 {
+                 	docsData.map(function(doc, i){
+                 	if (doc.type ==="added" ){
+                 		return <a key={i} style={{color: '#fff'}} href={doc.url}><Column large={3} className="doc_bloc">
+                 		   <h3 title={doc.name}>{doc.symbol}</h3>
                  		</Column></a>
-                 	} else{
-                 		return <a key={i} style={{color: '#fff'}} href={thang.url}><Column large={3} className="doc_bloc">
-                 		   <img className="doc_logo" title={thang.name} src={thang.logo}/>
+                 	} else {
+                 		return <a key={i} style={{color: '#fff'}} href={doc.url}><Column large={3} className="doc_bloc">
+                 		   <img className="doc_logo" title={doc.name} src={doc.logo}/>
                  		</Column></a>
                  	}
-                     
-                  })} 
+                  })
+                 } 
                </div>
             </div>
             )
@@ -185,8 +159,8 @@ class Docs extends Component {
       	return (
       		<div className="docs_component">
       		   <p style={{'cursor': 'pointer'}} onClick={this.toggleOn}>Hide</p>
-      		   <input className="addTitleInput" type="text" onKeyPress={this.handleInput} placeholder="Name"/>
-      		   <input className="addLinkInput" type="text" onKeyPress={this.handleInput} placeholder="Link"/>
+      		   <input className="addTitleInput" type="text" placeholder="Name"/>
+      		   <input className="addLinkInput" type="text" placeholder="Link"/>
       		   <div style={{'color':'#fff', 'cursor': 'pointer'}} onClick={this.saveInput}>save</div>
       		</div>
    		)
@@ -195,16 +169,15 @@ class Docs extends Component {
             	return (
             		<div className="docs_component">
             		   <p style={{'cursor': 'pointer'}} onClick={this.toggleOn}>Hide</p>
-            		   <div style={{'color':'#fff', 'cursor': 'pointer'}} onClick={this.saveInput}>save</div>
+            		   <div style={{'color':'#fff', 'cursor': 'pointer', marginBottom: '10px'}} onClick={this.saveInput}>save</div>
             		   <div className="inner_scroll">
-	            		  
 	            		   <ul>
 	            		   	{
-		            		   		docsData.map(function(doc, i){
-		            		   			if (doc.type === 'added'){
-		            		   				return <li className={'userDoc'+i} key={'doc'+ i} style={{'color':'#fff'}} onClick={this.docDelete}>{doc.name}<img src="src/assets/docs_logos/grey/delete.png" className="docDeleteButton" /></li>
-		            		   			}
-			            		   	}.bind(this))
+	            		   		docsData.map(function(doc, i){
+	            		   			if (doc.type === 'added'){
+	            		   				return <li className={'userDoc'+i} key={'doc'+ i} style={{'color':'#fff'}} onClick={this.docDelete}>{doc.name}<img src="src/assets/docs_logos/grey/delete.png" className="docDeleteButton" /></li>
+	            		   			}
+		            		   	}.bind(this))
 	            		   	}
 	            		   </ul>
             		   </div>
