674789d6bc8bdb83090298515e8cb5d98b4c04fc
Nathan Broaddus <nathanbroaddus@gmail.com>
2017-02-23 12:46:34 -0500

    [Dev-Dash] darkening bg photos with css overlay

--- src/components/App.js
+++ src/components/App.js
@@ -32,6 +32,7 @@ class App extends Component {
       return (
          <div className="application">
             <div className="bgImage">
+            <div className="underlay"></div>
                <MainDate/>
                <Search/>
                <News/>
