4755672728418d79f7083976c8ce7224f986c553
Nathan Broaddus <nathanbroaddus@gmail.com>
2017-02-28 17:33:03 -0500

    [Dev-Dash] updating github component, styles, refactor, still needs alot of tlc, working prototype

--- src/components/Github.js
+++ src/components/Github.js
@@ -27,6 +27,7 @@ class Github extends Component {
       console.log(this.state.user_data);
       if ( localStorage.user_name !== undefined && localStorage.user_name !== "undefined" ){
          this.getRequest(localStorage.user_name);
+         this.setState({on: true});
       }
    }
 
@@ -104,12 +105,12 @@ class Github extends Component {
    render() {
       if (this.state.on && this.state.user_data === null){
          return (
-            <div className="github_component"> 
+            <div className="github_component" style={{'cursor': 'pointer'}}> 
                <div className="gh_on">
-                 <p className="gh_title"  onClick={this.handleClick}>On</p>
-                 <input type="text" placeholder="Enter your Github Username" style={{width: '230px'}}/>
-                 <div className="save_gh_input" style={{'text-align':'right', 'position': 'absolute', 'bottom': '20px'}} onClick={this.saveInput}>Save</div>
-                 <div className="save_gh_input" style={{'text-align':'right', 'position': 'absolute', 'bottom': '0'}} onClick={this.changeUser}>Change User</div>
+                 <p className="gh_title title" style={{'float': 'right'}} onClick={this.handleClick}>Hide</p>
+                 <input type="text" placeholder="Enter your Github Username" style={{width: '230px', 'position': 'absolute', 'top': '50px'}}/>
+                 <div className="save_gh_input  title" style={{'text-align':'right', 'position': 'absolute', 'bottom': '20px', 'cursor': 'pointer', 'font-size':'20px'}} onClick={this.saveInput}>Save</div>
+
                </div>
             </div>
          )
@@ -118,15 +119,15 @@ class Github extends Component {
          return (
             <div className="github_component" > 
                <div className="gh_on">
-                 
+                <p className="gh_title title" style={{'float': 'right'}} onClick={this.handleClick}>Hide</p>
                  <a href={this.state.user_data.url}><img src={this.state.user_data.av}  style={{width: '120px'}} alt="" target="_blank"/></a>
-                 <ul style={{'position': 'absolute', 'top':'10px', 'left': '120px', 'listStyleType':'none'}}>
+                 <ul style={{'position': 'absolute', 'top':'25px', 'left': '120px', 'listStyleType':'none'}}>
                     <h5>{this.state.user_data.user}</h5>
-                    <li>Following : {this.state.user_data.following}</li>
-                    <li>Followers : {this.state.user_data.followers}</li>
-                    <li>Public Repos : {this.state.user_data.pub_repos}</li>
+                    <a href={"https://github.com/"+this.state.user_data.user+"?tab=following"}><li>Following : {this.state.user_data.following}</li></a>
+                    <a href={"https://github.com/"+this.state.user_data.user+"?tab=followers"}><li>Followers : {this.state.user_data.followers}</li></a>
+                    <a href={"https://github.com/"+this.state.user_data.user+"?tab=repositories"}><li>Public Repos : {this.state.user_data.pub_repos}</li></a>
                  </ul>
-                 <p className="gh_title" style={{'position':'absolute', 'bottom': '10px', 'opacity': '0.7', 'cursor': 'pointer'}} onClick={this.changeUser}>Change User</p>
+                 <p className="gh_title title" style={{'position':'absolute', 'bottom': '10px', 'opacity': '0.7', 'cursor': 'pointer'}} onClick={this.changeUser}>Change User</p>
                </div>
             </div>
          )
@@ -135,7 +136,8 @@ class Github extends Component {
          return (
             <div className="github_component" onClick={this.handleClick}> 
                <div className="gh_off">
-                 <p className="gh_title">Off</p>
+                 <p className="gh_title title">Github +</p>
+
               </div>
             </div>
          )
