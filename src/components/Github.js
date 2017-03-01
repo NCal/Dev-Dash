53281f79bb6e468c0f21b05f4886bec5f5d80dc3
Nathan Broaddus <nathanbroaddus@gmail.com>
2017-03-01 00:52:38 -0500

    [Dev-Dash] updating github component, refactoring, styling

--- src/components/Github.js
+++ src/components/Github.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Column } from 'react-foundation';
-import Foundation from 'react-foundation';
 import $ from 'jQuery';
-import GitHub from 'github-api';
-
 
 class Github extends Component {
    constructor(props) {
@@ -16,15 +12,11 @@ class Github extends Component {
       this.state = {
          on: false,
          user_data: null,
-         user_name: null,
          user_repo_data: null
       };
-      
    }
 
    componentDidMount() {
-      console.log('github mounted');
-      console.log(this.state.user_data);
       if ( localStorage.user_name !== undefined && localStorage.user_name !== "undefined" ){
          this.getRequest(localStorage.user_name);
          this.setState({on: true});
@@ -32,14 +24,17 @@ class Github extends Component {
    }
 
    handleClick(){
-      console.log('handle click');
       this.setState({'on': !this.state.on});
    }
 
-   saveInput(){
+   saveInput(e){
       let username = $('.gh_on input').val();
-      localStorage.user_name = username;
-      this.getRequest(username);
+      if (e.key === 'Enter' && username !== ''){this.setUserName(username);} 
+      if (e.type === 'click' && username !== ''){ this.setUserName(username);}
+   }
+
+   setUserName(username){
+      localStorage.user_name = username; this.getRequest(username);
    }
 
    changeUser(){
@@ -50,12 +45,12 @@ class Github extends Component {
    getRequest(username){
       console.log('USERNAME', username);
       let self = this;
-      // Run request automatically if data is already in local storage
       $.getJSON('https://api.github.com/users/'+ username, 
          {client_id: '28e07835843f7977ad64',
           client_secret: 'c971128144f02c7f97f96fdf45bf2649424cb46b'
          }, 
          function(res){
+          console.log('response',res);
             let user_data = {
                user : res.login,
                id: res.id,
@@ -81,19 +76,19 @@ class Github extends Component {
                client_id: '28e07835843f7977ad64',
                client_secret: 'c971128144f02c7f97f96fdf45bf2649424cb46b'
             }, 
-            function(resp){
+            function(res){
                let repos = [];
-               resp.forEach(function(repo, i){
+               res.forEach(function(repo, i){
                   let repo_data = {
-                     name: resp[i].name,
-                     description: resp[i].description,
-                     open_issues: resp[i].open_issues,
-                     url: resp[i].html_url,
-                     last_push: resp[i].pushed_at,
+                     name: res[i].name,
+                     description: res[i].description,
+                     open_issues: res[i].open_issues,
+                     url: res[i].html_url,
+                     last_push: res[i].pushed_at,
                   };
                   repos.push(repo_data);
                });
-               // console.log('repos', repos);
+
                localStorage.user_repo_data = JSON.stringify(repos);
                self.setState({user_repo_data: JSON.parse(localStorage.user_repo_data)});
 
@@ -105,12 +100,11 @@ class Github extends Component {
    render() {
       if (this.state.on && this.state.user_data === null){
          return (
-            <div className="github_component" style={{'cursor': 'pointer'}}> 
+            <div className="github_component"> 
                <div className="gh_on">
-                 <p className="gh_title title" style={{'float': 'right'}} onClick={this.handleClick}>Hide</p>
-                 <input type="text" placeholder="Enter your Github Username" style={{width: '230px', 'position': 'absolute', 'top': '50px'}}/>
-                 <div className="save_gh_input  title" style={{'text-align':'right', 'position': 'absolute', 'bottom': '20px', 'cursor': 'pointer', 'font-size':'20px'}} onClick={this.saveInput}>Save</div>
-
+                 <p className="gh_title title gh_hide" onClick={this.handleClick}>Hide</p>
+                 <input className="user_name_input" type="text" placeholder="Enter your Github Username" onKeyDown={this.saveInput}/>
+                 <div className="save_gh_input  title" onClick={this.saveInput}>Save</div>
                </div>
             </div>
          )
@@ -119,15 +113,15 @@ class Github extends Component {
          return (
             <div className="github_component" > 
                <div className="gh_on">
-                <p className="gh_title title" style={{'float': 'right'}} onClick={this.handleClick}>Hide</p>
-                 <a href={this.state.user_data.url}><img src={this.state.user_data.av}  style={{width: '120px'}} alt="" target="_blank"/></a>
-                 <ul style={{'position': 'absolute', 'top':'25px', 'left': '120px', 'listStyleType':'none'}}>
+                <p className="gh_title title gh_hide" onClick={this.handleClick}>Hide</p>
+                 <a href={this.state.user_data.url}><img src={this.state.user_data.av}  className="gh_av" alt="" target="_blank"/></a>
+                 <ul className="user_data_ul">
                     <h5>{this.state.user_data.user}</h5>
                     <a href={"https://github.com/"+this.state.user_data.user+"?tab=following"}><li>Following : {this.state.user_data.following}</li></a>
                     <a href={"https://github.com/"+this.state.user_data.user+"?tab=followers"}><li>Followers : {this.state.user_data.followers}</li></a>
                     <a href={"https://github.com/"+this.state.user_data.user+"?tab=repositories"}><li>Public Repos : {this.state.user_data.pub_repos}</li></a>
                  </ul>
-                 <p className="gh_title title" style={{'position':'absolute', 'bottom': '10px', 'opacity': '0.7', 'cursor': 'pointer'}} onClick={this.changeUser}>Change User</p>
+                 <p className="gh_title title gh_change_user" onClick={this.changeUser}>Change User</p>
                </div>
             </div>
          )
@@ -136,8 +130,7 @@ class Github extends Component {
          return (
             <div className="github_component" onClick={this.handleClick}> 
                <div className="gh_off">
-                 <p className="gh_title title">Github +</p>
-
+                 <h5 className="gh_title">Github +</h5>
               </div>
             </div>
          )
@@ -145,7 +138,5 @@ class Github extends Component {
       
    }
 }
-
-
 
 export default Github;
