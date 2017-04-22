fdc65c5b7d3052fd2f35c9df841ed8fb5c676137
Nathan Broaddus <nathanbroaddus@gmail.com>
2017-04-22 16:05:15 -0400

    [devDash] new build 0.1.0

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,7 @@ class App extends Component {
   }
    componentDidMount() {
       this.changeBackground();
+      // this.getBg();
       setTimeout(function(){
          $('.overlay').fadeOut(200);
       },30);
@@ -32,6 +33,36 @@ class App extends Component {
       let today = new Date;
       let todayNum = today.getDate();
       $('.bg_image').css({'background-image': 'url(src/assets/bg_photos/'+todayNum+'.jpg)'});
+   }
+
+   getBg(){
+    console.log('get bg');
+    // $.get('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key={86aa1ecca9719d0d668bf726a8401b9f}&tags=flower&per_page=3&format=json')
+    $.get('https://api.flickr.com/services/rest/?method=flickr.photos.search',
+      {
+      api_key:'86aa1ecca9719d0d668bf726a8401b9f',
+      format:'json',
+      tags: 'landscape, nature', 
+      extras: ', original_format, o_dims, url_o',
+      privacy_filter: 5
+
+    },
+      function(response){
+        response = response.replace('jsonFlickrApi(', '');
+        response = response.replace('})', '}');
+        response = JSON.parse(response);
+        // console.log(response);
+        response.photos.photo.map(function(thing){
+          // console.log(thing);
+          // if (Number(thing.width_o) > 2000){
+            console.log(thing);
+            console.log('small', 'https://farm'+thing.farm+'.staticflickr.com/'+thing.server+'/'+thing.id+'_'+thing.secret+'.jpg');
+            console.log('original size',thing.url_o);
+          // }
+          // https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+        });
+      });
+
    }
    render(){
       return (
