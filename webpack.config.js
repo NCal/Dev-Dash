c2f5b041de5a5b6a0f4dc19dc8c4e59ec3bb1a3a
Nathan Broaddus <nbroaddus@Neptune.local>
2017-12-26 22:18:39 -0500

    [devdash] refactoring

--- webpack.config.js
+++ webpack.config.js
@@ -47,28 +47,28 @@
 //     path: __dirname + '/build'
 //   }
 // }
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.join(__dirname, 'client'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-    {
-      test: /.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: {
-        presets: [ "es2015", "react", "stage-0"]
-      }
-    }, 
-    {
-      test: /\.scss$/,
-      loader: 'style-loader!css-loader!sass-loader?sourceMap'
-    },]
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: [ 'es2015', 'react', 'stage-0']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader?sourceMap'
+      } ]
   }
-};
+}
